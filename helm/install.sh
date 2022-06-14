cb6c148d2cf16b2ba697674d7a929644101266f5
‘likunkun‘ <likunkun@haier.com>
2022-06-14 20:29:59 +0800

    基础服务部署

--- helm/install.sh
+++ helm/install.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
 
 namespace=$1
+
 sed -i 's/zone-eureka/${namespace}/g' *.yaml
 sed -i 's/zone-eureka/${namespace}/g' rocketmq/*.yaml
 
+kubectl create namespace ${namespace}
 kubectl apply -f mockserver.yaml
 kubectl apply -f mysql.yaml
 kubectl apply -f nacos.yaml
@@ -19,4 +22,5 @@ kubectl apply -f rokcetmq/operator.yaml
 kubectl apply -f rokcetmq/rocketmq_v1alpha1_rocketmq_cluster.yaml
 kubectl apply -f rokcetmq/rocketmq.yaml
 
+sed -i 's/${namespace}/zone-eureka/g' *.yaml
 sed -i 's/${namespace}/zone-eureka/g' rocketmq/*.yaml
