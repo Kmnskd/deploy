f17b4a4d2327a778300f4e7c73238981eb45a50c
Kmnskd <40328160+Kmnskd@users.noreply.github.com>
2022-07-20 18:48:45 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,8 +1,10 @@
 TAG_NAME = {"1": "test", "2": "dev", "3": "feature", "4": "bloom",
             "5": "hope", "6": "grace", "7": "ack_feature", "8": "peace",
-            "9": "pearl", "10": "passion", "11": "eureka"}
+            "9": "pearl", "10": "passion", "11": "eureka", "12": "apple",
+            "13": "orange", "14": "peach"}
 
-ACK = ("hope", "grace", "ack_feature", "peace", "pearl", "passion", "eureka")
+ACK = ("hope", "grace", "ack_feature", "peace", "pearl",
+       "passion", "eureka", "apple", "orange", "peach", "bloom")
 
 HARBOR_IP = "10.163.205.239:8081"
 
@@ -16,7 +18,10 @@ NACOS_NAMESPACE = {"test": "796c847f-d808-4b1e-b79f-f6943c730604",
                    "peace": "f7da4f72-58f0-48e1-9a58-fbf7a67e41d7",
                    "pearl": "14501f0c-d97a-4813-9919-dc707abf7c8e",
                    "passion": "dd8f036e-8ac0-4d35-9204-a37226e03a09",
-                   "eureka": "3370c562-427d-4e8c-9c1a-0876d93361bb"}
+                   "eureka": "3370c562-427d-4e8c-9c1a-0876d93361bb",
+                   "apple": "9299bb7f-24b6-499b-ad33-305ec44cde26",
+                   "orange": "cb11d51f-8f84-449e-b8c1-016decf144d5",
+                   "peach": "9a1f3c11-ce41-4019-beeb-67bee5d93cf7"}
 
 SERVICE_INFO = {"baseline-auth": {"service_port": 20121, "node_port": {"dev": "30920", "test": "30921", "prd": "30922", "feature": "32700"}},
                 "baseline-project": {"service_port": 20111, "node_port": {"dev": "30923", "test": "30924", "prd": "30925", "feature": "32701"}},
